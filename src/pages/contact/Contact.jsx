c7cb8f8848c8886d76211d987e124be89d2da8cf
Kamal Paudel <techitus07@gmail.com>
2024-04-08 19:20:19 +0545

    basis of react finished

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -3,6 +3,7 @@ import Button from "../../components/Button"
 const Contact = () => {
   const [count, setCount] = useState(0)
   const [count2, setCount2] = useState(0)
+  const [count3, setCount3] = useState(0)
   // useEffect (()=>{
 
   //   console.log("Contact page loaded")
@@ -18,9 +19,11 @@ const Contact = () => {
         <h1>This is contact page</h1>
         <h2>{count}</h2>
         <h3>{count2}</h3>
-        <button onClick={()=>setCount(count+1)}>Increase</button>
+        <h2>{count3}</h2>
+        <button onClick={()=>setCount(count+1)}>Increase</button> <br />
    <Button />
-   <button onClick={()=>setCount2(count2-1)}>Decrease</button>
+   <button onClick={()=>setCount2(count2-1)}>Decrease</button> <br />
+<button onClick={()=>setCount3(count3 +2 )}>Increase by 2</button>
     </div>
   )
 }
